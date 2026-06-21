f34e853bd30827aaebf17970cbb3b3fdd31f0615
vreddyjyo-dev <vreddyjyo@gmail.com>
2026-06-21 15:30:34 +0530

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,28 @@
+int=2
+print(int)
+
+a=34
+print(a)
+
+float=0.14365
+print(float)
+
+b=0.67595
+print(b)
+
+String=("coding is the best")
+print(String)
+
+c=("I like codingal")
+print(c)
+
+Boolean=True
+print(Boolean)
+
+d=False
+print(d)
+
+print("Hello \n") 
+print("Hi")
+
+print("Hello",2)
